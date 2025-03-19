82f6f2eed148ee4eab343237027975ab3d67d7fc
rxy94 <rxia@freepik.com>
2025-03-19 12:04:38 +0100

    Creando la función Create del CRUD

--- app/public/index.php
+++ app/public/index.php
@@ -21,6 +21,59 @@ if ($characterId !== null) {
         'status' => 'success',
         'message' => "Personaje con ID {$characterId} eliminado correctamente"
     ]);
+}
+
+# Obtenemos los parámetros de creación del personaje de la URL si existen
+$name = isset($_GET['name']) ? $_GET['name'] : null;
+$birthDate = isset($_GET['birth_date']) ? $_GET['birth_date'] : null;
+$kingdom = isset($_GET['kingdom']) ? $_GET['kingdom'] : null;
+$equipmentId = isset($_GET['equipment_id']) ? (int)$_GET['equipment_id'] : null;
+$factionId = isset($_GET['faction_id']) ? (int)$_GET['faction_id'] : null; 
+
+# Si se pasa el parámetro create, creamos un nuevo personaje
+if (isset($_GET['create'])) {
+
+    try {
+        # Validamos que todos los parámetros estén presentes
+        if (!$name || !$birthDate || !$kingdom || !$equipmentId || !$factionId ) {
+            throw new Exception('Todos los parámetros son obligatorios');
+        }
+
+        # Validamos el formato de la fecha
+        $date = DateTime::createFromFormat('Y-m-d', $birthDate);
+        if (!$date) {
+            throw new Exception('El formato de fecha debe ser YYYY-MM-DD');
+        }
+
+        # Validamos que el equipamiento exista
+        $equipmentQuery = $db->prepare('SELECT id FROM equipments WHERE id = :id');
+        $equipmentQuery->execute(['id' => $equipmentId]);
+        if (!$equipmentQuery->fetch()) {
+            throw new Exception('El equipamiento especificado no existe');
+        }
+
+        # Validamos que la facción exista
+        $factionQuery = $db->prepare('SELECT id FROM factions WHERE id = :id');
+        $factionQuery->execute(['id' => $factionId]);
+        if (!$factionQuery->fetch()) {
+            throw new Exception('La facción especificada no existe');
+        }
+
+        # Creamos un nuevo personaje
+        createCharacter($name, $birthDate, $kingdom, $equipmentId, $factionId, $db);
+
+        echo json_encode([
+            'status' => 'success',
+            'message' => 'Personaje creado correctamente'
+        ]);
+
+    } catch (Exception $e) {
+        
+        echo json_encode([
+            'status' => 'error',
+            'message' => $e->getMessage()
+        ]);
+    }
 
 }
 
@@ -28,9 +81,8 @@ if ($characterId !== null) {
 $query = $db->query('SELECT * FROM characters');
 $characters = $query->fetchAll(PDO::FETCH_ASSOC);
 
-echo json_encode([
-    "characters" => $characters,
-]);
+echo json_encode($characters);
 
 # Cerramos la conexión a la base de datos
 $db = null;
+
