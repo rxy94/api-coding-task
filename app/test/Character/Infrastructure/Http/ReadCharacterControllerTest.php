31cbdf7551c8621e14f8b7d7aba8b262a3c0058a
rxy94 <rxia@freepik.com>
2025-04-02 10:26:11 +0200

    Add acceptance test 'ReadCharacterControllerTest.php' to verify character retrieval flow
    - Added an acceptance test to ensure that the ReadCharacterController properly retrieves a character by ID and returns the data as JSON.
    - The test simulates a full request-response cycle, including saving a character, sending a GET request to the character endpoint, and verifying the response.
    - Validated that the response matches the expected JSON format with character data and a success message.

--- app/test/Character/Infrastructure/Http/ReadCharacterControllerTest.php
+++ app/test/Character/Infrastructure/Http/ReadCharacterControllerTest.php
@@ -2,7 +2,21 @@
 
 namespace App\Test\Character\Infrastructure\Http;
 
+use App\Character\Domain\Character;
+use App\Character\Domain\CharacterRepository;
+use App\Character\Domain\CharacterToArrayTransformer;
+
+use DI\ContainerBuilder;
+use Dotenv\Dotenv;
 use PHPUnit\Framework\TestCase;
+use Slim\App;
+use Slim\Factory\AppFactory;
+use Slim\Psr7\Uri;
+use Slim\Psr7\Headers;
+use Slim\Psr7\Factory\StreamFactory;
+use Slim\Psr7\Request as SlimRequest;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
 
 class ReadCharacterControllerTest extends TestCase
 {
@@ -13,6 +27,71 @@ class ReadCharacterControllerTest extends TestCase
      */
     public function givenARequestToTheControllerWithOneCharacterIdWhenReadCharacterThenReturnTheCharacterAsJson()
     {
-        //TODO: Implementar
+        $app = $this->getAppInstance();
+
+        $repository = $app->getContainer()->get(CharacterRepository::class);
+
+        $expectedCharacter = new Character(
+            'John Doe',
+            '1990-01-01',
+            'Kingdom of Spain',
+            1,
+            1
+        );
+
+        $savedCharacter = $repository->save($expectedCharacter);
+
+        $request = $this->createRequest('GET', '/character/' . $savedCharacter->getId());
+        $response = $app->handle($request);
+
+        $payload = (string) $response->getBody();
+        $serializedPayload = json_encode([
+            'character' => CharacterToArrayTransformer::transform($savedCharacter),
+            'message' => 'Personaje obtenido correctamente'
+        ]);
+
+        $this->assertEquals($serializedPayload, $payload);
+
     }
+
+    private function getAppInstance(): App
+    {
+        $dotenv = Dotenv::createImmutable(__DIR__ . '/../../../../');
+        $dotenv->load();
+
+        $containerBuilder = new ContainerBuilder();
+
+        $settings = require __DIR__ . '/../../../../config/definitions.php';
+        $settings($containerBuilder);
+
+        $container = $containerBuilder->build();
+
+        AppFactory::setContainer($container);
+        $app = AppFactory::create();
+
+        $routes = require __DIR__ . '/../../../../config/routes.php';
+        $routes($app);
+
+        return $app;
+    }
+
+    private function createRequest(
+        string $method,
+        string $path,
+        array $headers = ['HTTP_ACCEPT' => 'application/json'],
+        array $cookies = [],
+        array $serverParams = []
+    ): Request {
+        $uri = new Uri('', '', 80, $path);
+        $handle = fopen('php://temp', 'w+');
+        $stream = (new StreamFactory())->createStreamFromResource($handle);
+
+        $h = new Headers();
+        foreach ($headers as $name => $value) {
+            $h->addHeader($name, $value);
+        }
+
+        return new SlimRequest($method, $uri, $h, $cookies, $serverParams, $stream);
+    }
+
 }
